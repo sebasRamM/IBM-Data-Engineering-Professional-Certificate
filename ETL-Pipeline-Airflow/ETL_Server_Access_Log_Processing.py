838eca99616b639933342a789431387c6e39cc92
Sebastian Ramirez Marulanda <107895948+sebasRamM@users.noreply.github.com>
2025-08-26 19:07:59 -0600

    ETL Data Pipeline Scripts

---
+++ ETL-Pipeline-Airflow/ETL_Server_Access_Log_Processing.py
@@ -0,0 +1,109 @@
+# Import the libraries
+from datetime import timedelta
+# The DAG object; we'll need this to instantiate a DAG
+from airflow.models import DAG
+# Operators; you need this to write tasks!
+from airflow.operators.python import PythonOperator
+
+# This makes scheduling easy
+from airflow.utils.dates import days_ago
+import requests
+
+input_file = 'web-server-access-log.txt'
+extracted_file = 'extracted-data.txt'
+transformed_file = 'transformed.txt'
+output_file = 'capitalized.txt'
+
+def get_file():
+    url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Apache%20Airflow/Build%20a%20DAG%20using%20Airflow/web-server-access-log.txt"
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
+        with open(input_file, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+print(f"File dowloaded successfully: {input_file}")
+
+def extract():
+    global input_file
+    print('Inside Extract')
+    with open(input_file, 'r') as infile, \
+            open(extracted_file, 'w') as outfile:
+        for line in infile:
+            fields = line.split('#')
+            if len(fields) >= 4:
+                field_1 = fields[0]
+                field_4 = fields[3]
+                outfile.write(field_1 + "#" + field_4 + "\n")
+
+def transform():
+    global extracted_file, transformed_file
+    print("Inside Transform")
+    with open(extracted_file, 'r') as infile, \
+            open(transformed_file, 'w') as outfile:
+        for line in infile:          
+            processed_line = line.upper()
+            outfile.write(processed_line + '\n')
+
+def load():
+    global transformed_file, output_file
+    print("Inside  Load")
+    with open(transformed_file, 'r') as infile, \
+            open(output_file, 'w') as outfile:
+        for line in infile:
+            outfile.write(line +  '\n')
+
+def check():
+    global output_file
+    print("Inside Check")
+    # Save the array to a CSV file
+    with open(output_file, 'r') as infile:
+        for line in infile:
+            print(line)
+
+default_args = {
+    'owner': 'Your name',
+    'start_date': days_ago(0),
+    'email': ['your email'],
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+# Define the DAG
+dag = DAG(
+    'my-first-python-etl-dag',
+    default_args=default_args,
+    description='My first DAG',
+    schedule_interval=timedelta(days=1),
+)
+# Define the task named download to call the `download_file` function
+download = PythonOperator(
+    task_id='download',
+    python_callable=get_file,
+    dag=dag,
+)
+# Define the task named execute_extract to call the `extract` function
+execute_extract = PythonOperator(
+    task_id='extract',
+    python_callable=extract,
+    dag=dag,
+)
+# Define the task named execute_transform to call the `transform` function
+execute_transform = PythonOperator(
+    task_id='transform',
+    python_callable=transform,
+    dag=dag,
+)
+# Define the task named execute_load to call the `load` function
+execute_load = PythonOperator(
+    task_id='load',
+    python_callable=load,
+    dag=dag,
+)
+# Define the task named execute_load to call the `load` function
+execute_check = PythonOperator(
+    task_id='check',
+    python_callable=check,
+    dag=dag,
+)
+# Task pipeline
+download >> execute_extract >> execute_transform >> execute_load >> execute_check
+
